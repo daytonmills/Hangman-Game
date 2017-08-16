6026472e3a9c88d90032b6981f3d0bf3caf40311
Dayton Mills <dayton@statscraft.net>
2017-08-16 12:41:45 -0700

    Finishing game logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,18 +3,49 @@
         Wild-West Hangman Game
 
 ***************************************/
-
-// Global Variables
-var word, wordObject, wrong=0, guesses=12;
-
-// Global Arrays
+var word;
+var wordObject;
 var wordArray;
 var wrongLetters=[];
+var wrong=0;
+var guesses=12;
+var wins=0;
+var loss=0;
+var running;
 
-initGame();
-
-function initGame()
+function gameOver()
 {
+    console.log("Game Over");
+    running=false;
+    loss+=1;
+    console.log(loss);
+    $("#loss").html(loss);
+    gameReset();
+}
+
+function gameWin()
+{
+    console.log("Game win")
+    running=false;
+    wins+=1;
+    console.log(wins);
+    $("#wins").html(wins);
+    gameReset();
+}
+
+function gameReset()
+{
+    guesses=12;
+    $('#guesses').html(guesses);
+    $('#letters-word').html("");
+    $('#letters-guessed').html("");
+}
+
+function gameInit()
+{
+    console.log("Game Init");
+    running=true;
+
     word = getWord();
     wordArray = getWordArray();
     wordObject = {
@@ -22,52 +53,57 @@ function initGame()
         wordArray: wordArray
     };
 
-    runGame();
-}
+    function getWord()
+    {
+        const dictionary = ["Cowboy", "Horse", "Desert", "Cactus", "Saloon", "Holster", "Spurs", "Lasso"];
+        const randomIndex = Math.floor(Math.random() * dictionary.length);
+        const randomWord = dictionary[randomIndex];
 
-function runGame()
-{
-    console.log(wordObject);
+        return randomWord;
+    }
+
+    function getWordArray()
+    {
+        let splitWordArray = word.split('');
+        return splitWordArray;
+    }
+
     drawSpaces();
-}
-
-function getWord()
-{
-    const dictionary = ["Cowboy", "Horse", "Desert", "Cactus", "Saloon", "Holster", "Spurs", "Lasso"];
-    const randomIndex = Math.floor(Math.random() * dictionary.length);
-    const randomWord = dictionary[randomIndex];
-
-    return randomWord;
-}
-
-function getWordArray()
-{
-    let splitWordArray = word.split('');
-    return splitWordArray;
+    console.log(wordObject);
 }
 
 function drawSpaces()
 {
     for(i = 0; i < wordObject.wordArray.length; i++)
     {
-        $('#currentWord').append("<span id=letter" + i + ">_ </span>");
+        $('#letters-word').append("<span id=letter-" + i + ">_ </span>");
     }
 }
 
 function drawLetter(letter, position)
 {
-    $('#letter' + position).html(letter + " ");
+    $('#letter-' + position).html(letter);
+
+    var wordPlayed = $('#letters-word').text();
+    if(wordPlayed == wordObject.word)
+    {
+        gameWin();
+    }
 }
 
 function drawWrong(letter)
 {
     var letterGuessed = $('#wrong-' + letter).text().indexOf(letter) > -1;
-
     if(wrongLetters.includes(letter) && !letterGuessed)
     {
         guesses--;
-        $('#letters').append("<span id=wrong-" + letter + ">" + letter + " </span>");
+        $('#letters-guessed').append("<span class=wrong-" + letter + ">" + letter + " </span>");
         $('#guesses').html(guesses);
+
+        if(guesses === 0)
+        {
+            gameOver();
+        }
     }
 }
 
@@ -81,27 +117,33 @@ function checkWrong(letter)
 }
 
 document.onkeyup = function(event) {
-    var input = String.fromCharCode(event.keyCode).toLowerCase();
-
-    for(let i = 0; i < wordObject.wordArray.length; i++)
+    if(running)
     {
-        if(input === wordObject.wordArray[i].toLowerCase())
+        var input = String.fromCharCode(event.keyCode).toLowerCase();
+
+        for(let i = 0; i < wordObject.wordArray.length; i++)
         {
-            drawLetter(wordObject.wordArray[i], i);
-            wrong = 0;
+            if(input === wordObject.wordArray[i].toLowerCase())
+            {
+                drawLetter(wordObject.wordArray[i], i);
+                wrong = 0;
+            }
+            else
+            {
+                wrong = wrong+1;
+                checkWrong(input);
+            }
+            if( i === wordObject.wordArray.length -1)
+            {
+                wrong = 0;
+            }
         }
-        else
-        {
-            wrong = wrong+1;
-            checkWrong(input);
-        }
-        if( i === wordObject.wordArray.length -1)
-        {
-            wrong = 0;
-        }
+    }
+    else if(!running){
+        gameInit();
+        console.log("Game Key");
     }
 }
 
-//If run out of guesses declare the game over, add a loss, and pick a new word (reset the board)
 
 //If user guesses all letters correctly, add a win and display winning animation (reset the board)
